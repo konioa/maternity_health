4e7a5a87e86987534cae050351b32d4abbda8bcd
konioa <goodsong0715@gmail.com>
2023-04-30 21:39:52 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'tops', to: 'tops#index'
+  root to: 'articles#index'
+  #resources :users, only: [:edit, :update]
+  resources :articles
+end
